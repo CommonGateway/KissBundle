e4e8c4e637d9aaca4a23a92418988e7989e0d1da
Wilco Louwerse <wilco@conduction.nl>
2023-06-13 10:43:21 +0200

    more cleanup

--- DependencyInjection/KissExtension.php
+++ DependencyInjection/KissExtension.php
@@ -8,9 +8,20 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class KissExtension extends Extension
 {
+
+
+    /**
+     * Load.
+     *
+     * @param array $configs
+     * @param ContainerBuilder $container
+     *
+     * @return void
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
-    }
-}
+
+    }//end load()
+}//end class
