6b3ab42d1b5ddc0cd9f319837d6572ac99cf2a17
Barry Brands <barrybrands02@hotmail.com>
2024-01-10 16:33:59 +0100

    Improvements and fixes for sync

--- Service/SyncOpenPDCService.php
+++ Service/SyncOpenPDCService.php
@@ -2,41 +2,90 @@
 
 namespace Kiss\KissBundle\Service;
 
-use App\Entity\Entity;
-use App\Service\EavService;
-use App\Exception\GatewayException;
 use App\Service\SynchronizationService;
 use CommonGateway\CoreBundle\Service\CallService;
 use CommonGateway\CoreBundle\Service\GatewayResourceService;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use CommonGateway\CoreBundle\Service\MappingService;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Cache\CacheException;
 use Psr\Cache\InvalidArgumentException;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\HttpFoundation\Response;
 
 class SyncOpenPDCService
 {
-    private EntityManagerInterface $entityManager;
+    /**
+     * @var GatewayResourceService
+     */
     private GatewayResourceService $resourceService;
+
+    /**
+     * @var CallService
+     */
     private CallService $callService;
+
+    /**
+     * @var SynchronizationService
+     */
     private SynchronizationService $syncService;
+
+    /**
+     * @var MappingService
+     */
     private MappingService $mappingService;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+
+    /**
+     * @var SymfonyStyle|null
+     */
+    private ?SymfonyStyle $style = null;
+
+    /**
+     * @var array
+     */
     private array $data;
+
+    /**
+     * @var array
+     */
     private array $configuration;
 
     public function __construct(
-        EntityManagerInterface $entityManager,
         GatewayResourceService $resourceService,
         CallService $callService,
         SynchronizationService $syncService,
-        MappingService $mappingService
+        MappingService $mappingService,
+        EntityManagerInterface $entityManager
     ) {
-        $this->entityManager = $entityManager;
         $this->resourceService = $resourceService;
         $this->callService = $callService;
         $this->syncService = $syncService;
         $this->mappingService = $mappingService;
+        $this->entityManager = $entityManager;
     }
+
+
+    /**
+     * Set symfony style in order to output to the console.
+     *
+     * @param SymfonyStyle $style
+     *
+     * @return self
+     *
+     * @todo change to monolog
+     */
+    public function setStyle(SymfonyStyle $style): self
+    {
+        $this->style = $style;
+
+        return $this;
+
+    }//end setStyle()
 
     /**
      * Handles the synchronization of the openpub api.
@@ -50,16 +99,19 @@ class SyncOpenPDCService
      */
     public function syncOpenPDCHandler(array $data, array $configuration): array
     {
+        $this->style && $this->style->info('SyncOpenPDC triggered');
+
         $this->data = $data;
         $this->configuration = $configuration;
 
-        $source = $this->resourceService->getSource($this->configuration['burenSource'], 'klantinteractie-servicesysteem/kiss-bundle');
-        $schema = $this->resourceService->getSchema($this->configuration['pubSchema'], 'klantinteractie-servicesysteem/kiss-bundle');
-        // $mapping = $this->resourceService->getMapping($this->configuration['pubMapping'], 'klantinteractie-servicesysteem/kiss-bundle');
+        $source = $this->resourceService->getSource($this->configuration['source'], 'common-gateway/kiss-bundle');
+        $schema = $this->resourceService->getSchema($this->configuration['schema'], 'common-gateway/kiss-bundle');
+        $mapping = $this->resourceService->getMapping($this->configuration['mapping'], 'common-gateway/kiss-bundle');
         $endpoint = $this->configuration['endpoint'];
 
         $sourceConfig = $source->getConfiguration();
 
+        $this->style && $this->style->info('Fetching objects..');
         $response = $this->callService->getAllResults(
             $source,
             $endpoint,
@@ -68,13 +120,18 @@ class SyncOpenPDCService
 
         $responseItems = [];
         foreach ($response as $result) {
-            dump($result);die;
+            $result = $this->mappingService->mapping($mapping, $result);
+
             $synchronization = $this->syncService->findSyncBySource($source, $schema, $result['id']);
             // $synchronization->setMapping($mapping);
             $synchronization = $this->syncService->synchronize($synchronization, $result);
+            $this->entityManager->persist($synchronization);
 
             $responseItems[] = $synchronization->getObject()->toArray();
         }
+        $this->entityManager->flush();
+
+        $this->style && $this->style->success('Synchronized ' . count($responseItems) . ' objects');
 
         $this->data['response'] = new Response(json_encode($responseItems), 200);
 
